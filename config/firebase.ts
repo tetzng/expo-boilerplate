e61ddb5ad535057052f2c523648b3a22c0501a00
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-13 21:49:45 +0900

    set env

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,7 +5,12 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apikey...
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
