8bbcb89ff6a76fbe8840d62e25f389ec4ab00757
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-29 03:18:11 +0900

    create user document

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,9 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp()
+
+export const createUserDoc = functions.auth.user().onCreate((user) => {
+  admin.firestore().collection('users').doc(user.uid)
+})
