0637731725c038853d787dcdec4c222b4b7dd615
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-29 03:18:32 +0900

    delete comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,6 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 admin.initializeApp()
 
 export const createUserDoc = functions.auth.user().onCreate((user) => {
